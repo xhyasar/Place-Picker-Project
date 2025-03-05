ccdb4b4b1399ab85b7d91e71461b01aaa198c8e2
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-03-05 17:24:47 +0300

    Refactor handleRemovePlace to use useCallback for performance optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState, useEffect} from 'react';
+import {useRef, useState, useEffect, useCallback} from 'react';
 
 import Places from './components/Places.jsx';
 import {AVAILABLE_PLACES} from './data.js';
@@ -56,18 +56,20 @@ function App() {
         }
     }
 
-    function handleRemovePlace() {
-        setPickedPlaces((prevPickedPlaces) =>
-            prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
-        );
-        setModalIsOpen(false);
 
-        const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-        localStorage.setItem(
-            'selectedPlaces',
-            JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
-        );
-    }
+
+     const handleRemovePlace = useCallback(function handleRemovePlace() {
+            setPickedPlaces((prevPickedPlaces) =>
+                prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+            );
+            setModalIsOpen(false);
+
+            const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+            localStorage.setItem(
+                'selectedPlaces',
+                JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+            );
+            }, []);
 
     return (
         <>
